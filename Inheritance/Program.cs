2bfaeac68b3bc5291b225b1d05a6cc269c653f8f
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-01-26 22:05:56 +0300

    26 Ocak

---
+++ Inheritance/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName = "Akif"
+                },
+                new Student
+                {
+                    FirstName = "Irmak"
+                },
+                new Person
+                {
+                    FirstName = "Ali"
+                }
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+    interface IPerson
+    {
+
+    }
+
+    //Classların tek başlarına anlamı vardır ama interfacelerin tek başlarına bir anlamı yoktur. İkisi de inheritance gibi kullanılabiliyor.
+    //Aslında interfaceler bir implementasyondur. Fakat yeni nesil dillerde interfacelerin inheritance gibi kullanım senaryoları söz konusudur.
+    //Bir sınıfa ebeveyn atarken ve interface atarken önce sınıf yani inheritance, sonra interface veya interfaceler(kaç tane varsa) altaki örnekte
+    class Customer : Person, IPerson
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
---
+++ Interfaces2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Interfaces2
+{
+    class Program
+    {
+        //interface new'lenemez.
+        static void Main(string[] args)
+        {
+            //interface ler onu implemente eden class'ın referansı numarasını (referansını) tutabilirler.
+            //IPersonManager customerManager = new CustomerManager();
+            //customerManager.Add();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            //employeeManager.Update();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new EmployeeManager());
+            projectManager.Add(new InternManager());
+            projectManager.Update(new CustomerManager());
+        }
+    }
+
+    interface IPersonManager
+    {
+        //unimplemented operation 
+        void Add();
+        void Update();
+    }
+
+    class PersonManager
+    {
+        //implemented operation
+        public void AddPerson()
+        {
+            Console.WriteLine("Eklendi");
+        }
+    }
+
+    //inherits - class -------------- implements - interface
+    class CustomerManager : PersonManager, IPersonManager
+    {
+        public void Add()
+        {
+            //müşteri ekleme kodları
+            Console.WriteLine("Müşteri Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //personel ekleme kodları
+            Console.WriteLine("Personel Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel Güncellendi");
+        }
+    }
+
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer Güncellendi");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager) //parametre olarak interface verdik. Dikkat...
+        {
+            personManager.Add();
+        }
+
+        public void Update(IPersonManager personManager)
+        {
+            personManager.Update();
+        }
+    }
+}
