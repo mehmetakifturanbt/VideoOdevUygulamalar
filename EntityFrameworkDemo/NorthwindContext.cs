e560b28c6c4086226b615012177beb810973f7ce
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-02-07 23:15:32 +0300

    7 Şubat 2021

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+    //public class A
+    //{
+    //    public virtual void Do()
+    //    {
+    //        //
+    //        //
+    //        //
+    //    }
+    //}
+
+    //public class B : A
+    //{
+    //    public override void Do()
+    //    {
+    //        base.Do();
+    //    }
+    //}
+}
